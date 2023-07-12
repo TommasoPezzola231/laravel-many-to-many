e144e19728a5d5502e563c8cfa114ec7a50dc600
TommasoPezzola <pezzola123@gmail.com>
2023-07-12 16:12:32 +0200

    add Migration/Seeder for Technology

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 10; $i++) { 
+            $newType = new Technology();
+            $newType->name = $faker->word();
+            $newType->save();
+        }
+    }
+}
